c8135826b8c05d4ef538d5acf90296f5f3e23687
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-04-24 12:47:07 -0300

    ➕ Adding Comments

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -4,14 +4,19 @@ import ButtonStatusBar from './ButtonStatusBar'
 
 import './StatusBar.css'
 
+// Array com as configurações do arquivo .env
+// referentes aos botões da status bar.
 const statusBarButtonsConfig = JSON.parse(process.env.REACT_APP_CONFIGURACAO_BOTOES_STATUS_BAR)
+
+// Renderiza a status bar. Todavia, o botão é exibido
+// em seu próprio componente, o ButtonStatusBar,
+// recebendo os valores desse componente.
 
 export default class StatusBar extends Component {
   render() {
     return (
       <div className="status-bar">
         {statusBarButtonsConfig.map((button) => {
-          console.log(button)
           return (
             <ButtonStatusBar
               key={button.titulo}
