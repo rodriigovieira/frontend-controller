6f7d12dfef6ec6b63a161e2c0005c44f61e00faf
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-04-24 12:47:40 -0300

    ➕ Adding Comments and Log Messages

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react"
 
-import './Logs.css'
+import "./Logs.css"
+
+// Aqui estão todos os registros do sistema.
+// A cada novo acesso criado, um log é recebido aqui.
+
+// Todas as informações que são renderizadas, são recebidas
+// através dos "props", que são acessados com "this.props".
 
 export default class Logs extends Component {
   render() {
     return (
       <div className="logs">
-        <h1>Logs</h1>
+        {this.props.messageList &&
+          this.props.messageList.map(message => (
+            <Fragment key={message.usuarioId}>
+              <p key={message.usuarioNome}>{message.time} - {message.userId} - {message.userName}</p>
+            </Fragment>
+          ))}
       </div>
     )
   }
