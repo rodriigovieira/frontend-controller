e2513d886d2cf504952ecb524dae0a4c102137d5
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-04-24 12:46:58 -0300

    ➕ Adding Comments

--- src/components/ButtonStatusBar.js
+++ src/components/ButtonStatusBar.js
@@ -2,6 +2,11 @@ import React, { Component, Fragment } from 'react'
 
 import './ButtonStatusBar.css'
 
+// Responsável por exibir os botões da Status Bar.
+// Esse componente recebe a função de liberação através dos props,
+// pois a função é definida no componente pai,
+// sendo apenas executada nesse componente.
+
 export default class ButtonStatusBar extends Component {
   render() {
     return (
